0191c5b3f03002af554c783458627cb96e82579d
David-IFC <disloker1494@gmail.com>
2026-02-12 13:55:06 +0100

    Update index.php

--- Davine/index.php
+++ Davine/index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Restaurante</title>
     <meta name="description">
-    <base href="/b/">
+   <!--  <base href="/b/"> -->
     <link rel="stylesheet" href="style.css">
 </head>
 
