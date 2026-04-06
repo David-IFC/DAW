5b3f418681f6ab5048e9e8693b6ae185b25bceeb
David <disloker1494@gmail.com>
2026-04-06 13:19:51 +0200

    asd

--- AplicacionFinalCursoPHP/assets/db/db.php
+++ AplicacionFinalCursoPHP/assets/db/db.php
@@ -11,4 +11,21 @@ try {
 } catch (PDOException $e) {
     die("Error de conexión: " . $e->getMessage());
 }
+
+/* codigo para db en infinityfree:
+
+<?php
+$host = "sql104.infinityfree.com";
+$db = "if0_41205019_local";
+$user = "if0_41205019";
+$pass = "2yfjysppfu80FZ";
+
+try {
+    $conexion = new PDO("mysql:host=$host;dbname=$db;charset=utf8mb4", $user, $pass);
+    $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+} catch (PDOException $e) {
+    die("Error de conexión: " . $e->getMessage());
+}
+?> */
 ?>
